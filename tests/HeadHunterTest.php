e19ba00599147332867ea7cf0436dd223848123a
Alex Masterov <alex.masterow@gmail.com>
2017-06-24 10:27:01 +0300

    Improve tests

--- tests/HeadHunterTest.php
+++ tests/HeadHunterTest.php
@@ -1,161 +1,155 @@
 <?php
+declare(strict_types=1);
 
-namespace AlexMasterov\OAuth2\Client\Tests\Provider;
+namespace AlexMasterov\OAuth2\Client\Provider\Tests;
 
-use AlexMasterov\OAuth2\Client\Provider\Exception\HeadHunterException;
-use AlexMasterov\OAuth2\Client\Provider\HeadHunter;
-use Eloquent\Phony\Phpunit\Phony;
-use GuzzleHttp\ClientInterface;
-use League\OAuth2\Client\Token\AccessToken;
+use AlexMasterov\OAuth2\Client\Provider\{
+    HeadHunter,
+    HeadHunterException,
+    HeadHunterResourceOwner,
+    Tests\CanAccessTokenStub,
+    Tests\CanMockHttp
+};
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\ResponseInterface;
 
 class HeadHunterTest extends TestCase
 {
-    /**
-     * @var HeadHunter
-     */
-    private $provider;
-
-    protected function setUp()
-    {
-        $this->provider = new HeadHunter([
-            'clientId'     => 'mock_client_id',
-            'clientSecret' => 'mock_secret',
-            'redirectUri'  => 'mock_redirect_uri',
-        ]);
-    }
-
-    protected function tearDown()
-    {
-        parent::tearDown();
-    }
-
-    protected function mockResponse($body)
-    {
-        $response = Phony::mock(ResponseInterface::class);
-        $response->getHeader->with('content-type')->returns('application/json');
-        $response->getBody->returns(json_encode($body));
-
-        return $response;
-    }
-
-    protected function mockClient(ResponseInterface $response)
-    {
-        $client = Phony::mock(ClientInterface::class);
-        $client->send->returns($response);
-
-        return $client;
-    }
+    use CanAccessTokenStub;
+    use CanMockHttp;
 
     public function testAuthorizationUrl()
     {
-        // Run
-        $url = $this->provider->getAuthorizationUrl();
-        $path = \parse_url($url, PHP_URL_PATH);
+        // Execute
+        $url = $this->provider()
+            ->getAuthorizationUrl();
 
         // Verify
-        $this->assertSame('/oauth/authorize', $path);
+        self::assertSame('/oauth/authorize', path($url));
     }
 
     public function testBaseAccessTokenUrl()
     {
-        $params = [];
+        static $params = [];
 
-        // Run
-        $url = $this->provider->getBaseAccessTokenUrl($params);
-        $path = \parse_url($url, PHP_URL_PATH);
+        // Execute
+        $url = $this->provider()
+            ->getBaseAccessTokenUrl($params);
 
         // Verify
-        $this->assertSame('/oauth/token', $path);
+        self::assertSame('/oauth/token', path($url));
+    }
+
+    public function testResourceOwnerDetailsUrl()
+    {
+        // Stub
+        $apiUrl = $this->apiUrl();
+        $tokenParams = [
+            'access_token'  => 'mock_access_token',
+        ];
+
+        $accessToken = $tokenParams['access_token'];
+
+        // Execute
+        $detailUrl = $this->provider()
+            ->getResourceOwnerDetailsUrl($this->accessToken($tokenParams));
+
+        // Verify
+        self::assertSame(
+            "{$apiUrl}/me?access_token={$accessToken}",
+            $detailUrl
+        );
     }
 
     public function testDefaultScopes()
     {
-        $reflection = new \ReflectionClass(get_class($this->provider));
-        $getDefaultScopesMethod = $reflection->getMethod('getDefaultScopes');
-        $getDefaultScopesMethod->setAccessible(true);
+        $getDefaultScopes = function () {
+            return $this->getDefaultScopes();
+        };
 
-        // Run
-        $scope = $getDefaultScopesMethod->invoke($this->provider);
-
-        // Verify
-        $this->assertEquals([], $scope);
-    }
-
-    public function testGetAccessToken()
-    {
-        // https://github.com/hhru/api/blob/master/docs/authorization.md
-        $body = [
-            'access_token'  => 'mock_access_token',
-            'token_type'    => 'bearer',
-            'expires_in'    => \time() * 3600,
-            'refresh_token' => 'mock_refresh_token',
-        ];
-
-        $response = $this->mockResponse($body);
-        $client = $this->mockClient($response->get());
-
-        // Run
-        $this->provider->setHttpClient($client->get());
-        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+        // Execute
+        $defaultScopes = $getDefaultScopes->call($this->provider());
 
         // Verify
-        $this->assertNull($token->getResourceOwnerId());
-        $this->assertEquals($body['access_token'], $token->getToken());
-        $this->assertEquals($body['refresh_token'], $token->getRefreshToken());
-        $this->assertGreaterThanOrEqual($body['expires_in'], $token->getExpires());
+        self::assertSame([], $defaultScopes);
     }
 
-    public function testUserProperty()
+    public function testCheckResponse()
     {
-        $body = [
-            'id'          => 12345678,
-            'last_name'   => 'lst_name',
-            'first_name'  => 'first_name',
-            'middle_name' => 'middle_name',
-            'email'       => 'email',
-        ];
+        $getParseResponse = function () use (&$response, &$data) {
+            return $this->checkResponse($response, $data);
+        };
 
-        $tokenOptions = [
-            'access_token' => 'mock_access_token',
-            'expires_in'   => 3600,
-        ];
-
-        $token = new AccessToken($tokenOptions);
-        $response = $this->mockResponse($body);
-        $client = $this->mockClient($response->get());
-
-        // Run
-        $this->provider->setHttpClient($client->get());
-        $user = $this->provider->getResourceOwner($token);
-
-        // Verify
-        $this->assertEquals($body['id'], $user->getId());
-        $this->assertEquals($body['last_name'], $user->getLastName());
-        $this->assertEquals($body['first_name'], $user->getFirstName());
-        $this->assertEquals($body['middle_name'], $user->getMiddleName());
-        $this->assertArrayHasKey('email', $user->toArray());
-    }
-
-    public function testErrorResponses()
-    {
+        // Stub
         $code = 400;
-        $body = [
+        $data = [
             'error'             => 'Foo error',
             'error_description' => 'Error description',
         ];
 
-        $response = $this->mockResponse($body);
-        $response->getStatusCode->returns($code);
-        $client = $this->mockClient($response->get());
+        // Mock
+        $response = $this->mockResponse('', '', $code);
 
-        $this->expectException(HeadHunterException::class);
-        $this->expectExceptionCode($code);
-        $this->expectExceptionMessage(implode(': ', $body));
+        // Verify
+        self::expectException(HeadHunterException::class);
+        self::expectExceptionCode($code);
+        self::expectExceptionMessage(implode(': ', $data));
 
-        // Run
-        $this->provider->setHttpClient($client->get());
-        $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+        // Execute
+        $getParseResponse->call($this->provider());
     }
+
+    public function testCreateResourceOwner()
+    {
+        $getCreateResourceOwner = function () use (&$response, &$token) {
+            return $this->createResourceOwner($response, $token);
+        };
+
+        // Stub
+        $token = $this->accessToken();
+        $response = [
+            'id'           => random_int(1, 1000),
+            'last_name'   => 'mock_last_name',
+            'first_name'  => 'mock_first_name',
+            'middle_name' => 'mock_middle_name',
+            'email'       => 'mock_email',
+        ];
+
+        // Execute
+        $resourceOwner = $getCreateResourceOwner->call($this->provider());
+
+        // Verify
+        self::assertInstanceOf(HeadHunterResourceOwner::class, $resourceOwner);
+        self::assertEquals($response['id'], $resourceOwner->getId());
+        self::assertEquals($response['last_name'], $resourceOwner->getLastName());
+        self::assertEquals($response['first_name'], $resourceOwner->getFirstName());
+        self::assertEquals($response['middle_name'], $resourceOwner->getMiddleName());
+        self::assertSame($response, $resourceOwner->toArray());
+    }
+
+    private function provider(...$args): HeadHunter
+    {
+        static $default = [
+            'clientId'     => 'mock_client_id',
+            'clientSecret' => 'mock_secret',
+            'redirectUri'  => 'mock_redirect_uri',
+        ];
+
+        $values = array_replace($default, ...$args);
+
+        return new HeadHunter($values);
+    }
+
+    private function apiUrl(): string
+    {
+        $getApiUrl = function () {
+            return $this->urlApi;
+        };
+
+        return $getApiUrl->call($this->provider());
+    }
+}
+
+function path(string $url): string
+{
+    return parse_url($url, PHP_URL_PATH);
 }
